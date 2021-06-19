5800b3ad12f9fd196971d6d9ef62dcbd9fb9a2fc
Luis Antonio Preza Padilla <anibulusnn@gmail.com>
2021-06-18 22:17:54 -0500

    Refragmentación del código
    Se reorganiza el código del archivo principal para que sea más legible

--- Program.cs
+++ Program.cs
@@ -17,24 +17,9 @@ namespace Consola
                 bool opc=true;
                 
                 do
-                {
-                    WriteLine("Favor de ingresar la ruta, nombre incluyendo su extensión del archivo en ese orden o especificar carpeta terminanco con \"\\\".");    
-                    WriteLine("Ruta");
-                    ruta=ReaderHelper.ValidarRuta(@ReadLine());
-                    if(ruta!="")
-                    {
-                        WriteLine("Nombre del archivo");
-                        string nombre=ReaderHelper.ValidarNombreDeArchivo(ReadLine());
-                        if(nombre!="")
-                        {
-                            string[] file=nombre.Split(".");
-                            ch.archivos.Add(new Archivo(file[0], file.Length>1?"."+file[1]:"", ruta));
-                            WriteLine("Se ha agregado el archivo a la lista");
-                        }
-                    }
-                    
+                {                    
                     WriteLine(@"¿Desea agregar otro archivo o carpeta? (S\N)");
-                    opc=(ReadLine().Equals("s"));
+                    opc=(ReadLine().Equals("s",StringComparison.OrdinalIgnoreCase));
                 }while(opc);//Fin de ciclo para listado de archivos
 
                 WriteLine("¿Cada cuánto tiempo desea que se realice? (Nm Ns Nd)");
@@ -50,6 +35,46 @@ namespace Consola
             }
 
         }//Fin de main
+
+        ///<SUMMARY>
+        ///Primero busca la ruta con un formato valido
+        ///para después invocar  el método que pide el nombre del archivo
+        private static void InputGetRuta(CopyHelper ch, string ruta)
+        {
+            do
+            {
+                WriteLine("Favor de ingresar la ruta, nombre incluyendo su extensión del archivo en ese orden o especificar carpeta terminanco con \"\\\".");    
+                WriteLine("Ruta");
+                ruta=ReaderHelper.ValidarRuta(@ReadLine());
+                if(ruta!="")
+                {
+                    InputGetNombreArchivo(ch, ruta);
+                }
+            }while(ruta=="");
+        }//fin de método ruta
+
+        ///<SUMMARY>
+        ///Obtiene el formato de la nombre de archivo hasta que se ingrese un formato válido 
+        private static void InputGetNombreArchivo(CopyHelper ch, string ruta)
+        {
+            string nombre;
+            do
+            {
+                WriteLine("Nombre del archivo");
+                nombre=ReaderHelper.ValidarNombreDeArchivo(ReadLine());
+
+                if(nombre!="")
+                {
+                    string[] file=nombre.Split(".");
+                    ch.archivos.Add(new Archivo(file[0], file.Length>1?"."+file[1]:"", ruta));
+                    WriteLine("Se ha agregado el archivo a la lista");
+                }
+                else
+                {
+                    WriteLine("El texto ingresado no tiene el formato de una ruta.");
+                }
+            }while(nombre=="");
+        }//Fin de método de nombre de archivo
 
     }//Fin de la clase
 
